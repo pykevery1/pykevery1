45e91769f8a6f32dac953370579672a019653a83
pykevery1 <62019758+pykevery1@users.noreply.github.com>
2025-01-30 22:33:48 -0800

    Update README.py
    I'm less dumber now

--- README.py
+++ README.py
@@ -1,25 +1,2 @@
-import random
-import time
-
-class Introduce:
-    Bleeps = ["&","#","$","-","*","+","@","_"]
-
-    def MyselfAs (name):
-        print("Hi! I'm " + str(name))
-        time.sleep(2)
-        print("I am a self taught programmer, who's only goal is to get better")
-        time.sleep(2)
-        print("I have made a few projects, and am great at python (for being self taught :P)")
-        time.sleep(2)
-        print("but I want to learn some web design, c++, c# for unity, and alot more!")
-        time.sleep(2)
-        print("enjoy your day, and remember:")
-        time.sleep(3)
-        while True:
-            G = ""
-            for I in range(0,4):
-                G = G + Introduce.Bleeps[random.randint(0,len(Introduce.Bleeps)-1)]
-            print(G + " programming")
-            time.sleep(0.2)
-
-Introduce.MyselfAs("kenny")
+Print('I am just some self taught programmer')
+Print('I know godot and python')
